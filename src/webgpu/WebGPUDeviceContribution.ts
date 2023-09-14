1df360f043bc823c56aa2c8f4d404ef861d8c004
yuqi.pyq <yuqi.pyq@antgroup.com>
2023-09-14 17:31:44 +0800

    feat: add WebGPU implementation

---
+++ src/webgpu/WebGPUDeviceContribution.ts
@@ -0,0 +1,64 @@
+import init, { glsl_compile } from '../../rust/pkg/glsl_wgsl_compiler';
+import { DeviceContribution } from '../api';
+import { Device_WebGPU } from './Device';
+
+export interface WebGPUDeviceOptions {
+  shaderCompilerPath: string;
+  onContextLost: () => void;
+}
+
+export class WebGPUDeviceContribution implements DeviceContribution {
+  constructor(
+    private pluginOptions: Partial<WebGPUDeviceOptions>,
+    private runtime: GlobalRuntime,
+  ) {}
+
+  async createSwapChain($canvas: HTMLCanvasElement) {
+    // eslint-disable-next-line
+    if ((globalThis.navigator as any).gpu === undefined) return null;
+
+    let adapter = null;
+    try {
+      // eslint-disable-next-line
+      adapter = await (globalThis.navigator as any).gpu.requestAdapter();
+    } catch (e) {
+      console.log(e);
+    }
+
+    if (adapter === null) return null;
+
+    // @see https://www.w3.org/TR/webgpu/#dom-gpudevicedescriptor-requiredfeatures
+    const optionalFeatures: GPUFeatureName[] = [
+      // 'depth24unorm-stencil8',
+      'depth32float-stencil8',
+      'texture-compression-bc',
+    ];
+    const requiredFeatures = optionalFeatures.filter((feature) =>
+      adapter.features.has(feature),
+    );
+    const device = await adapter.requestDevice({ requiredFeatures });
+
+    if (device) {
+      // @see https://github.com/gpuweb/gpuweb/blob/main/design/ErrorHandling.md#fatal-errors-requestadapter-requestdevice-and-devicelost
+      const { onContextLost } = this.pluginOptions;
+      device.lost.then(() => {
+        if (onContextLost) {
+          onContextLost();
+        }
+      });
+    }
+
+    if (device === null) return null;
+
+    const context = $canvas.getContext('webgpu');
+
+    if (!context) return null;
+
+    try {
+      await init(this.pluginOptions.shaderCompilerPath);
+    } catch (e) {}
+
+    // @ts-ignore
+    return new Device_WebGPU(adapter, device, $canvas, context, glsl_compile);
+  }
+}
