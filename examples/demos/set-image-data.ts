7150207a6edc907d526dafd658817d3c69653ccb
yuqi.pyq <yuqi.pyq@antgroup.com>
2024-01-02 14:04:07 +0800

    fix: support set image data in webgpu

--- examples/demos/set-image-data.ts
+++ examples/demos/set-image-data.ts
@@ -35,9 +35,18 @@ export async function render(
       unpackFlipY: false,
       packAlignment: 1,
     },
-    mipLevelCount: 0,
+    mipLevelCount: 1,
   });
   floatR.setImageData([new Float32Array([10.25])]);
+
+  const u8RGBA = device.createTexture({
+    format: Format.U8_RGBA_RT,
+    width: 1,
+    height: 1,
+    usage: TextureUsage.SAMPLED,
+    mipLevelCount: 1,
+  });
+  u8RGBA.setImageData([new Uint8Array([1, 1, 1, 1])]);
 
   // const floatRGB = device.createTexture({
   //   format: Format.F32_RGB,
@@ -65,7 +74,7 @@ export async function render(
 
 render.params = {
   targets: ['webgl1', 'webgl2', 'webgpu'],
-  default: 'webgl2',
+  default: 'webgpu',
   width,
   height,
 };
