1f610efb735b14d5ec0f6d23b9ae42aaf403ba0e
@thinkinggis <lzx199065@gmail.com>
2024-10-09 16:38:10 +0800

    feat: iswebGl2 fixed (#202) (#203)
    * feat: iswebGl2 fixed
    
    * fix: ts error fixed
    
    * fix: namespace change
    
    ---------
    
    Co-authored-by: XinyueDu <51403464+XinyueDu@users.noreply.github.com>
    Co-authored-by: duxinyue.dxy <duxinyue.dxy@antgroup.com>

--- src/webgl/utils.ts
+++ src/webgl/utils.ts
@@ -24,18 +24,23 @@ import type { Sampler_GL } from './Sampler';
 import type { Texture_GL } from './Texture';
 
 // @see https://github.com/visgl/luma.gl/blob/30a1039573576d73641de7c1ba222e8992eb526e/modules/gltools/src/utils/webgl-checks.ts#L22
+let isWebGL2Flag;
 export function isWebGL2(
   gl: WebGL2RenderingContext | WebGLRenderingContext,
 ): gl is WebGL2RenderingContext {
-  if (
-    typeof WebGL2RenderingContext !== 'undefined' &&
-    gl instanceof WebGL2RenderingContext
-  ) {
-    return true;
+
+  if(isWebGL2Flag !== undefined) {
+    return isWebGL2Flag;
+  }
+  if (typeof WebGL2RenderingContext !== 'undefined' &&
+      gl instanceof WebGL2RenderingContext) {
+      isWebGL2Flag = true;
+      return true;
   }
   // Look for debug contexts, headless gl etc
   // @ts-ignore
-  return Boolean(gl && gl._version === 2);
+  isWebGL2Flag = Boolean(gl && gl._version === 2);
+  return isWebGL2Flag;
 }
 
 export function isTextureFormatCompressed(fmt: Format): boolean {
